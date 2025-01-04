37964b77ebab5ecb98148c628f375c1aada32f2b
hebatullahgassem <hebatullahgassem@gmail.com>
2025-01-04 16:15:57 +0200

    jewellery v2

---
+++ jewelry/jewelry.js
@@ -0,0 +1,19 @@
+function navigateToAll() {
+    window.location.href = "./categories/all/all.html"; 
+}
+
+function navigateToBrac() {
+    window.location.href = "./categories/bracelets/bracelet.html"; 
+}
+
+function navigateToEar() {
+    window.location.href = "./categories/earings/earing.html"; 
+}
+
+function navigateToNeck() {
+    window.location.href = "./categories/necklesses/neckless.html"; 
+}
+
+function navigateToRing() {
+    window.location.href = "./categories/rings/ring.html"; 
+}
