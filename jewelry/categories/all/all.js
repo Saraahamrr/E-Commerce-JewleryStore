8a91a189f62c9e24179310e242fefb80361a8826
hebatullahgassem <hebatullahgassem@gmail.com>
2025-01-06 06:15:36 +0200

    view all jewellery page

---
+++ jewelry/categories/all/all.js
@@ -0,0 +1,64 @@
+fetch('../jewellery.json')
+.then(res => {
+    if(!res.ok){
+        throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return res.json();
+})
+.then(products => {
+    // Shuffle products array
+    const shuffleArray = (array) => {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+    };
+
+    const shuffledProducts = shuffleArray(products);
+
+    const container = document.getElementById('container');
+
+    const itemsCount = shuffledProducts.length;
+    const conter = document.createElement('p');
+    conter.textContent = `${itemsCount} PRODUCTS`; 
+    conter.className = 'conter';
+
+    // Cards wrapper
+    const cardsWrapper = document.createElement('div');
+    cardsWrapper.className = 'cards';
+
+    container.appendChild(conter);
+    container.appendChild(cardsWrapper);
+
+    products.forEach(product => {
+        const card = document.createElement('div');
+        card.className = 'card';
+
+        const img = document.createElement('img');
+        img.src = product.img;
+
+        const title = document.createElement('h3');
+        title.className = 'product-title';
+        title.textContent = product.title;
+
+        const price = document.createElement('p');
+        price.className = 'product-price';
+        price.textContent = `LE ${product.price}`;
+
+        const addToCart = document.createElement('button');
+        addToCart.className = 'addToCart';
+        addToCart.textContent = 'Add To Cart';
+        //handle addToCart feature here
+
+        card.appendChild(img);
+        card.appendChild(title);
+        card.appendChild(price);
+        card.appendChild(addToCart);
+
+        cardsWrapper.appendChild(card);
+    });
+})
+.catch(error => {
+    console.error('Error fetching the JSON file:', error);
+});
